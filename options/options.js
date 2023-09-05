cc4614546a4c139009fa918b3bb0bba8b7872a41
Crowdbotics <team@crowdbotics.com>
2023-09-05 17:06:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-shape-43239",
+  url: "https://hidden-shape-43239.botics.co",
+  api: "https://hidden-shape-43239.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
